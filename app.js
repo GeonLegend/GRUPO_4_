8eb6a1649a1b5f8c22868c4723e10b4c5f281416
LuisM435 <lx147812345@gmail.com>
2023-08-06 20:31:53 -0300

    Poniendo las rutas en el app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
 const PORT = process.env.PORT||4444;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get('/producto-detalle', (req, res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
+app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 app.listen(PORT, () => console.log('Escuchando puerto en el servidor: ' + PORT));
