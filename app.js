b2708301669c77536a6348feb3ed358f3db9d885
lautaro <magrey2015@gmail.com>
2023-08-21 21:07:13 -0300

    el register

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html
 app.get('/producto-detalle', (req, res) => res.sendFile(path.join(__dirname, 'views', 'productDetail.html')));
 app.get('/carrito', (req, res) => res.sendFile(path.join(__dirname, 'views', 'productCart.html')));
 app.get('/login', (req, res) => res.sendFile(path.join(__dirname, 'views', 'login.html')));
-app.get('/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname , 'views','register.html')));
 app.listen(PORT, () => console.log('Escuchando puerto en el servidor: ' + PORT));
