16707e0d8a05878ab14283177415e4b5b01cfbd7
LuisM435 <lx147812345@gmail.com>
2023-09-04 23:09:08 -0300

    Agregando los jsons de productos(con sus imagenes) y usuario. Y ademas modificado de model.js para manipular los json

--- data/model.js
+++ data/model.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
-const notesPath = path.join(__dirname, 'notas.json');
+const productsPath = path.join(__dirname, "products.json");
+const usersPath = path.join(__dirname, "users.json");
 
-module.exports = {
+let model = {
     getProduct : () => {
         const content = fs.readFileSync(notesPath, 'utf8');
         return JSON.parse(content || '[]')
@@ -12,5 +13,46 @@ module.exports = {
             notesPath, 
             JSON.stringify(notes, null, 2),
         )
-    }
+    },
+    
+
+    getProduct : () => {
+        /* Se le el contenido del json */
+        const content = fs.readFileSync(productsPath, 'utf8');
+        /* Se transforma el json a un objeto literal */
+        return JSON.parse(content || '[]')
+    },
+    WriteProduct : function(product) {
+        let products = this.getProduct();
+        products.push(product);
+
+        fs.writeFileSync(
+            /* Ruta del json */
+            productsPath, 
+            /* Se transforma el objeto literal que recibe como parametro a un json */
+            /* El tercer parametro son los espacios de lineas */
+            JSON.stringify(products, null, 2),
+        )
+    },
+
+    getUser : () => {
+        /* Se le el contenido del json */
+        const content = fs.readFileSync(usersPath, 'utf8');
+        /* Se transforma el json a un objeto literal */
+        return JSON.parse(content || '[]')
+    },
+    writeUser : function(user) {
+        let users = this.getUser();
+        users.push(user);
+
+        fs.writeFileSync(
+            /* Ruta del json */
+            usersPath, 
+            /* Se transforma el objeto literal que recibe como parametro a un json */
+            /* El tercer parametro son los espacios de lineas */
+            JSON.stringify(users, null, 2),
+        )
+    },
 }
+
+module.exports = model;
