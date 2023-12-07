f26b9386d905ddb5cb8f2e5a93fc5f5a333495c2
LuisM435 <lx147812345@gmail.com>
2023-12-07 11:26:48 -0300

    Moviendo el codigo a la carpeta src y Añadiendo la conexion con sequelize y modelos de la DB

---
+++ scripts/TechSmart.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE TechSmart;
+USE TechSmart;
+
+CREATE TABLE user_type(
+	id				bigint unsigned not null auto_increment,
+    name 			varchar(80) not null,
+    description		varchar(240) not null,
+	primary key(id)
+);
+
+CREATE TABLE category(
+	id				bigint unsigned not null auto_increment,
+    name			varchar(80) not null,
+    description		varchar(600),
+    primary key(id)
+);
+
+CREATE TABLE brand(
+	id 				bigint unsigned not null auto_increment,
+    name			varchar(300) not null,
+	description 	varchar(600),
+    id_product		bigint unsigned not null,
+    primary key(id)
+);
+
+CREATE TABLE users(
+	id				bigint unsigned not null auto_increment,
+    first_name		varchar(120) not null,
+    last_name 		varchar(80) not null,
+    password 		varchar(200) not null,
+    email 			varchar(120) not null,
+    gender 			varchar(1)	not null,
+    country 		varchar(80) not null,
+    date_of_birth	date not null,
+    avatar			varchar(80) not null,
+    id_user_type	bigint unsigned not null,
+    primary key(id),
+    foreign key(id_user_type) references user_type(id)
+);
+
+CREATE TABLE products(
+	id 				bigint unsigned not null auto_increment,
+    id_category		bigint unsigned not null,
+    id_brand		bigint unsigned not null,
+    name 			varchar(120) not null,
+    price 			float not null,
+    discount 		int unsigned default 0,
+    stock			int unsigned not null default 0,
+    description 	varchar(1800) not null,
+    warranty		int not null,
+    rating 			float unsigned not null,
+    img				varchar(300) not null,
+    primary key(id),
+    foreign key(id_category) references category(id),
+    foreign key(id_brand) references brand(id)
+);
+
+CREATE TABLE user_product(
+	id 				bigint unsigned not null auto_increment,
+    id_user			bigint unsigned not null,
+    id_product 		bigint unsigned not null,
+    date			date not null,
+    primary key(id),
+    foreign key(id_user) references users(id),
+    foreign key(id_product) references products(id)
+);
+
+CREATE TABLE orders(
+	id 				bigint unsigned not null auto_increment,
+    id_product 		bigint unsigned not null,
+    stock 			int unsigned not null,
+    total_price		float unsigned not null,
+    method_of_pay 	varchar(120) not null,
+    status 			varchar(30) not null,
+    date			date not null,
+    address			varchar(240) not null,
+    primary key(id),
+    foreign key(id_product) references products(id)
+);
+
+CREATE TABLE cart(
+	id 				bigint unsigned not null auto_increment,
+    id_user			bigint unsigned not null,
+    id_order		bigint unsigned not null,
+    primary key(id),
+    foreign key(id_user) references users(id),
+    foreign key(id_order) references orders(id)
+);
+
+CREATE TABLE sales(
+	id 				bigint unsigned not null auto_increment,
+    id_cart			bigint unsigned not null,
+    date			date not null,
+    primary key(id),
+    foreign key(id_cart) references cart(id)
+);
+
+
+
+
+
+
+
+
--- src/controllers/prueba.js
+++ src/controllers/prueba.js
@@ -3,8 +3,8 @@ const sequelize = db.sequelize;
 
 const prueba = {
     list: async function(req, res){
-        let users = await db.User.findAll({
-            include: ["tiposDeUsuario", "productos" ]
+        let users = await db.Product.findAll({
+            include: ["categorias", "marcas", "usuarios" ]
         });
         res.send(users);
     },
